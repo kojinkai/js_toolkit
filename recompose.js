1251892698f3107103af2cf98e77cd0f84acc2a1
Lewis Nixon <lewcfn@gmail.com>
2018-01-10 17:00:05 +0100

    add a recompose branch loadeer snippet

---
+++ recompose.js
@@ -0,0 +1,13 @@
+// React and Recompose branch loader pattern
+
+import { branch, renderComponent } from 'recompose';
+
+import UIComponent from './UIComponent';
+import Loader from './Loader';
+
+const withLoaderWhileLoading = branch(
+  ({ loading }) => loading === true,
+  renderComponent(Loader),
+);
+
+WithLoaderWhileLoading(ReceiptsColumnComponent);
